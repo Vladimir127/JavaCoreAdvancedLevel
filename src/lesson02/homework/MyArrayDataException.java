56db6c37853ff7a5139bc151ed435e0c57f91526
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2021-02-02 15:51:29 +0300

    Реализована логика.

--- src/lesson02/homework/MyArrayDataException.java
+++ src/lesson02/homework/MyArrayDataException.java
@@ -1,6 +1,13 @@
 package lesson02.homework;
 
-public class MyArrayDataException extends Throwable {
-    public MyArrayDataException(String s) {
+/** Исключение, возникающее, если не удалось преобразовать какой-либо элемент массива в целое число */
+public class MyArrayDataException extends Exception {
+
+    /**
+     * Инициализирует экземпляр класса
+     * @param message Сообщение об ошибке
+     */
+    public MyArrayDataException(String message) {
+        super(message);
     }
 }
--- src/lesson02/homework/exceptions/MySizeArrayException.java
+++ src/lesson02/homework/exceptions/MySizeArrayException.java
@@ -1,2 +1,13 @@
-package lesson02.homework.exceptions;public class MySizeArrayException {
+package lesson02.homework.exceptions;
+
+/** Исключение, возникающее при неверно заданном размере массива */
+public class MySizeArrayException extends Exception {
+
+    /**
+     * Инициализирует экземпляр класса
+     * @param message Сообщение об ошибке
+     */
+    public MySizeArrayException(String message) {
+        super(message);
+    }
 }
